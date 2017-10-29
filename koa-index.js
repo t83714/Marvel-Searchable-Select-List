f4b2fcb05c66123f8083b2ce6352548fb50ec7c3
Jacky Jiang <t83714@gmail.com>
2017-10-29 19:17:09 +1030

    Loading Image style

--- koa-index.js
+++ koa-index.js
@@ -1,5 +1,5 @@
 import app from "./server/app";
 
 app.listen(3000);
-console.log("listening on port 3000");
+console.log("Local Server listening on port 3000");
 console.log("Please access app from URL: http://localhost:3000/");
--- src/views/SearchBox.js
+++ src/views/SearchBox.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+const styles={
+    loadingIcon:{
+        right: "45px",
+        position: "absolute"
+    }
+};
+
 class SearchBox extends React.Component {
     render(){
         return (
-            <form className="form-inline my-2 my-lg-0">
+            <form className="select-box form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                <img src="loading.gif" className="loading-icon" width={24} height={24} />
             </form>
         );
     }
